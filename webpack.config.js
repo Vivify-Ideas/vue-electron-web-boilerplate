d386cb38a278e1ecb5a55698474bb1eb504f9c33
Nikola Spalevic <nikola.s@vivifyideas.com>
2016-12-16 13:24:41 +0000

    webpack version fix

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-if (process.argv.indexOf('--electron') > -1) {
-  module.exports = require('./config/webpack.electron');
-  return;
-}
-
-module.exports = require('./config/webpack.web');
